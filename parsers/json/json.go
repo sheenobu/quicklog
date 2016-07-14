c7323f3c4ed28816ddb24edceeb0061af2afcd30
Sheena Artrip <sheena.artrip@gmail.com>
2016-07-14 18:01:35 -0400

    parsers/json - cleanup JSON parser

--- parsers/json/json.go
+++ parsers/json/json.go
@@ -3,16 +3,19 @@ package json
 import (
 	"bytes"
 	"encoding/json"
+
 	"github.com/sheenobu/quicklog/ql"
 )
 
 func init() {
-	ql.RegisterParser("json", &JSONParser{})
+	ql.RegisterParser("json", &Parser{})
 }
 
-type JSONParser struct{}
+// Parser is a parser for JSON data.
+type Parser struct{}
 
-func (jp *JSONParser) Parse(buffer []byte, line *ql.Line, config map[string]interface{}) error {
+// Parse parses the given buffer and adds it to the line
+func (jp *Parser) Parse(buffer []byte, line *ql.Line, _ map[string]interface{}) error {
 	err := json.NewDecoder(bytes.NewReader(buffer)).Decode(&line.Data)
 	if err != nil {
 		return err
