ce8e188456e5b4bd8e5a305c4c281146e32e4c53
Sheena Artrip <sheena.artrip@gmail.com>
2016-08-23 10:14:44 -0400

    ql - provide adaptor functions for the common interface types

--- ql/inputs.go
+++ ql/inputs.go
@@ -6,6 +6,14 @@ import (
 	"golang.org/x/net/context"
 )
 
+var (
+	inputs map[string]InputFactory
+)
+
+func init() {
+	inputs = make(map[string]InputFactory)
+}
+
 // A Buffer is a series of characters and key-value data that get put on the pipeline
 type Buffer struct {
 	Data     []byte
@@ -17,30 +25,25 @@ type InputProcess interface {
 	Start(context.Context, chan<- Buffer) error
 }
 
+// InputProcessFunc is an adaptor for casting functions to InputProcess interfaces
+type InputProcessFunc func(context.Context, chan<- Buffer) error
+
+// Start starts the input processor by writing to the channel
+func (i InputProcessFunc) Start(ctx context.Context, ch chan<- Buffer) error {
+	return i(ctx, ch)
+}
+
 // InputFactory is the factory for building input processes given the config options
 type InputFactory interface {
 	Build(jsonConfig io.Reader) (InputProcess, error)
 }
 
 // InputFactoryHandler converts the given function to an InputFactory
-func InputFactoryHandler(fn func(io.Reader) (InputProcess, error)) InputFactory {
-	return &factfn{fn}
-}
+type InputFactoryHandler func(io.Reader) (InputProcess, error)
 
-type factfn struct {
-	fn func(io.Reader) (InputProcess, error)
-}
-
-func (f *factfn) Build(jsonConfig io.Reader) (InputProcess, error) {
-	return f.fn(jsonConfig)
-}
-
-var (
-	inputs map[string]InputFactory
-)
-
-func init() {
-	inputs = make(map[string]InputFactory)
+// Build builds the input process given the JSON config reader
+func (f InputFactoryHandler) Build(jsonConfig io.Reader) (InputProcess, error) {
+	return f(jsonConfig)
 }
 
 // GetInput gets the input driver
